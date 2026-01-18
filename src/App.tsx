9e717a443c8f21f6201ee5b95071e2b1e5186c38
Your Name <u513509754@gmail.com>
2026-01-18 12:51:32 -0700

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleAll = () => {
+    setError(null);
+    goodsAPI
+      .getAll()
+      .then(setGoods)
+      .catch(err => setError(err.message || 'Something went wrong'));
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const handleFirstFive = () => {
+    goodsAPI
+      .getFirstFive()
+      .then(setGoods)
+      .catch((err: { message: string | null }) =>
+        setError(err.message || 'Something went wrong'),
+      );
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleRedButton = () =>
+    goodsAPI
+      .getRedGoods()
+      .then(setGoods)
+      .catch(err => setError(err.message || 'Something went wrong'));
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      {error !== null && <div className="error">{error}</div>}
+
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" onClick={handleAll} data-cy="all-button">
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        onClick={handleFirstFive}
+        data-cy="first-five-button"
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" onClick={handleRedButton} data-cy="red-button">
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
--- src/types/Good.ts
+++ src/types/Good.ts
@@ -2,4 +2,6 @@ export interface Good {
   id: number;
   name: string;
   color: string;
+  get5First(): Promise<Good[]>;
+  getRedGoods(): Promise<Good[]>;
 }
